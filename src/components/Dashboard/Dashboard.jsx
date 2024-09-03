8d965396e8fe8e1e2de8430908255f25431dc19d
Andrew <andrewchau1993@gmail.com>
2024-09-03 16:15:52 -0700

    attempt to fix logo

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -5,7 +5,7 @@ const Dashboard = () => {
   const user = useContext(AuthedUserContext);
   return (
     <main>
-      <img src="../src/assets/TrafLogo.png" alt="Travel Log Logo" />
+      <img src="https://i.imgur.com/dmcbP3u.png" alt="Trafique Logo" />
       <h1>Welcome {user.user?.username}</h1>
       <p>
         This is the dashboard page where as you can see, your dreams of travel are now a journal. Safe Journeys!
--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,7 +1,7 @@
 const Landing = () => {
   return (
     <main>
-      <img src="../src/assets/TrafLogo.png" alt="Travel Log Logo" />
+      <img src="https://i.imgur.com/dmcbP3u.png" alt="Trafique Logo" />
       <h1>Hello, welcome to Trafique.</h1>
       <h3>
         If you sign up for a new account, you will have the ability to start journaling your life's journeys.
